1ffc2b1d1979f30969637208a4bb651aa94c926e
hemanthnvd <hemanthnvd@gmail.com>
2024-08-21 17:13:09 -0400

    fix: add ingress and dns records pointing to nginx-ingress load balancer

--- helm_release_ingress_nginx.tf
+++ helm_release_ingress_nginx.tf
@@ -11,3 +11,17 @@ resource "helm_release" "ingress_nginx" {
 
   depends_on = [kubernetes_namespace.llm-namespace]
 }
+
+resource "null_resource" "ingress" {
+  depends_on = [aws_route53_record.grafana_dns,aws_route53_record.llm_api_dns,aws_route53_record.llm_dns]
+  provisioner "local-exec" {
+    command = <<EOT
+    kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.8.0/cert-manager.crds.yaml
+    helm repo add jetstack https://charts.jetstack.io
+    helm repo update
+    helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.8.0 --create-namespace
+    kubectl apply -f cert
+    kubectl apply -f ingress
+    EOT
+  }
+}
